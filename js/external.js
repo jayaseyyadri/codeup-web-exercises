6cfa17dd2b04344146ee08476ffa1288d4e56958
jaya <jayaseyyadri@gmail.com>
2020-09-29 17:44:54 -0500

    Up to date external.js

--- js/external.js
+++ js/external.js
@@ -2,13 +2,13 @@
 
 console.log("Hello from external JavaScript.");
 
-alert("Welcome to my website");
+alert("Welcome to my Website!");
 
 var userInput = prompt("What is your favorite color ?");
 var alertMessage = "Great, " + userInput + " is my favorite color too !";
 alert(alertMessage);
 
-var welcome = console.log("Hello ");
+
 var dailyRentalRate = prompt("What is the daily rental rate?");
 var daysLittleMermaid = Number(prompt("How many days would you like to rent the little Mermaid ?"));
 var daysBrotherBear = Number(prompt("How many days would you like to rent the Brother Bear ?"));
@@ -20,7 +20,7 @@ var facebookRatePerHour = Number(prompt("What is your facebook hourly pay rate ?
 var googleRatePerHour = Number(prompt("What is your google hourly pay rate ?"));
 var amazonRatePerHour = Number(prompt("What is your amazon hourly pay rate ?"));
 var facebookWorkHours = Number(prompt("Please input number of hours worked at facebook : "));
-var googleWorkHours = Number(prompt("Please input number of hours worked : google"));
+var googleWorkHours = Number(prompt("Please input number of hours worked at google"));
 var amazonWorkHours = Number(prompt("Please input number of hours worked at amazon:"));
 
 var faceBookPay = (facebookWorkHours * facebookRatePerHour);
@@ -33,11 +33,11 @@ alert(" You total pay is  $ "+ totalPay.toFixed(3));
 var classNotFull = confirm("Class is not full");
 var noConflict = confirm("No conflicting schedule");
 var canEnroll = (classNotFull) && (noConflict);
-alert=(" Student Enrolled");
+alert=(" Student Enrolled" +canEnroll);
 
-var offerValid = confirm("is the offer valid ?");
-var isPremium = confirm("are you a premium user !");
-var itemNumber = Number(prompt("How many items do you have? "));
+var offerValid = confirm("Is the offer valid ?");
+var isPremium = confirm("Are you a premium user !");
+var numberOfItems = Number(prompt("How many items do you have? "));
 
-var discountApplied = (offerValid && (isPremium)|| itemNumber>2);
+var discountApplied = (offerValid && (isPremium)|| numberOfItems>2);
 alert("You qualify for discount ," + discountApplied);
