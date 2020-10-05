a7f34a525e3c8109b0d12514ce4e3176d8132fb9
jaya <jayaseyyadri@gmail.com>
2020-10-05 18:14:19 -0500

    modifying loops exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,20 @@
 
 do {
-	var check = prompt("Enter a number between 1 & 50");//ask for odd no 1-50
-}while (check %2===0);//number is not odd keep prompting
+	var userNumber = Number(prompt("Enter an odd number between 1 & 50"));
+	if(userNumber<1||userNumber >50){//ask for odd no 1-50
+	    alert("Invalid number, Please enter a number between 1 & 50")
+    }else if(userNumber%2===0){
+	    alert(userNumber+" is not odd , Please enter an odd number")
+    }else if(isNaN(userNumber)){
+	    alert(userNumber +" is not a number , please enter a number")
+    }else {
+        alert(userNumber + " is valid .Thank you!");
+        break;
+    }
+}while (true);//number is not odd keep prompting
+
+
+
 for(var i=1;i<50;i++) {
 	if (parseFloat(check)== i){
 		console.log("Yikes skipping " + check)
