6ef0d5a9de0743cbe7094b564ee5bc71c86897c0
jaya <jayaseyyadri@gmail.com>
2020-10-06 13:42:23 -0500

    completed planet arrays manipulation exercises

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,29 +19,30 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    console.log(planets.unshift("The Sun"));
+    console.log(planets.unshift());
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
-    console.log(planets.push("Pluto"));
+    console.log(planets.push());
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
-    console.log(planets.shift("The Sun"));
+    console.log(planets.shift());
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
-    console.log(planets.pop("Pluto"));
+    console.log(planets.pop());
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
     console.log(planets.indexOf("Earth"));
+    console.log(planets);
 
     console.log("Reversing the order of the planets array.");
     console.log(planets.reverse());
-    console.log(planets.reverse());
+    console.log(planets);
 
     console.log("Sorting the planets array.");
     console.log(planets.sort());
-
+    console.log(planets);
 })();
