e45c0c7d3bcdc3f21e5896eb499ab302840a6aa9
jaya <jayaseyyadri@gmail.com>
2020-10-09 10:30:48 -0500

    working on learning math object

---
+++ js/Matn-object-lec.js
@@ -0,0 +1,21 @@
+
+// // Math.random()
+// // var mathComputations = new.Math()//Don't do this  since math is static
+//
+// // let  randonNum=Math.random()//between 0 and 1 ie decimals
+// // randonNum=Math.random()*10//between 0 and 10
+// // randonNum=Math.random()*100// between 0 and 100
+// // console.log(randonNum);
+// // //split - split into array and find size and use that size to randomize
+// //
+// // console.log(Math.round(randonNum))  //between 0 and 3 ;can't use . asan index so round to nearest integer
+// // let index=Math.round(randonNum)*2
+// // console.log(Math.round(randonNum)*2)  //between 0 and 2
+//
+// let randomWord=inputArray[index];
+// console.log(randomWord);
+//Math.floor() - used to round down
+//
+// Math.ceil(randomWord);
+// randomWord=inptArray[index];///anything above 3.99 will be 4
+// randomWord=inptArray[index];///anything above 0.00001 will be 1
