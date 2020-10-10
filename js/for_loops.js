185a891def82686477de63dbfdb2815a757fe180
jaya <jayaseyyadri@gmail.com>
2020-10-10 00:53:41 -0500

    finished working on loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,29 +1,31 @@
-function showMultiplicationTable(number) {
+(function() {
+    function showMultiplicationTable(num) {
 
-    for (var i = 1; i <= 10; i++) {
+        for (var i = 1; i <= 10; i++) {
 
-      console.log(number + " * " + i + " = "+ number *i );
+            console.log(num + " * " + i + " = " + num * i);
+        }
     }
-}
-showMultiplicationTable(6);
 
-  for(var i=0;i<=10;i++) {
-   var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
-    var check =(randomNumber%2===0)?randomNumber +" is even ":randomNumber+" is odd ";
+    showMultiplicationTable(6);
+
+    for (var i = 0; i < 10; i++) {
+        var randomNumber = Math.floor((Math.random() * (200 - 20 + 1)) + 20);
+        var check = (randomNumber % 2 === 0) ? randomNumber + " is even " : randomNumber + " is odd ";
         console.log(check);
-  }
+    }
 
 
-    for(var i=0;i<=9;i++) {
-	var row ='';
-	for (var j = 0; j < i; j++) {
-		row+=i;
-	}
-	console.log(row);
-}
+    for (var i = 0; i <= 9; i++) {
+        var row = '';
+        for (var j = 0; j < i; j++) {
+            row += i;
+        }
+        console.log(row);
+    }
 
-    for (var i = 100; i > 0; i--){
-        if (i % 5 === 0) {
+    for (var i = 100; i >=5; i-=5) {
             console.log(i)
-        }}
+    }
+})();
 
