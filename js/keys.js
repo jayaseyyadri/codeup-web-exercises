46246e3f874807fa24a323a96f74b446b0933faf
jaya <jayaseyyadri@gmail.com>
2020-11-17 12:12:50 -0600

    working on weathermap functionality

--- js/keys.js
+++ js/keys.js
@@ -1,1 +1,5 @@
 var mapboxToken = 'pk.eyJ1IjoiamF5YXNleXlhZHJpIiwiYSI6ImNraDNmYnJ6dDA2b2sycnBmeWd3MWIwNGMifQ.10RvBz2N8KA5_MA1yy_BYw';
+const OPEN_WEATHER_APPID ="0169d7b27cce2d3d9871f1f0e867ab43";
+
+const newKey="b1f091412e1d832778c926e93b2a0488"
+
